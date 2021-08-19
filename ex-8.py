c793f4dbc373a59bb1a4041a212634492d5f2a89
venkata nihar billakurthi <84034294+venkataniharbillakurthi@users.noreply.github.com>
2021-08-19 11:56:37 +0530

    Create ex-8.py

---
+++ ex-8.py
@@ -0,0 +1,14 @@
+word=input('Enter the word')
+status=Flase
+for i in "aeiouAEIOU":
+    status=True
+if status==True:
+    print("word contains vowels")
+else:
+    print("word doesn't contains vowels")
+    
+    
+    
+#output
+#Enter the word:why
+#word contains vowels
