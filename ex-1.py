268665b865ba164ecef92b00cd1a82cfe2b43e5f
venkata nihar billakurthi <84034294+venkataniharbillakurthi@users.noreply.github.com>
2021-08-19 10:35:27 +0530

    Update ex-1.py

--- ex-1.py
+++ ex-1.py
@@ -1,3 +1,7 @@
 weight_in_kilo_gram = float(input("Enter weight in KG to convert into pounds:"))
 pounds = weight_in_kilo_gram*2.2
 print('weight in kilo gram = ' ,weight_in_kilo_gram,'\n weight in pounds = ',pounds )
+
+Enter weight in KG to convert into pounds:26
+weight in kilo gram =  26.0 
+ weight in pounds =  57.2
