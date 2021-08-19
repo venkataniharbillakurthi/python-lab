0c571675b1a46d46ffe0440a02a15cc032e68a08
venkata nihar billakurthi <84034294+venkataniharbillakurthi@users.noreply.github.com>
2021-08-19 10:32:23 +0530

    Create ex-4.py

---
+++ ex-4.py
@@ -0,0 +1,8 @@
+name =input('Enter the name:')
+n=int(input('How many times to print:'))
+print(name*n)
+
+
+Enter the name:nihar
+How many times to print:23
+niharniharniharniharniharniharniharniharniharniharniharniharniharniharniharniharniharniharniharniharniharniharnihar
