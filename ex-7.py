200a78f48a19664921a61183446eb45e13df7ea1
venkata nihar billakurthi <84034294+venkataniharbillakurthi@users.noreply.github.com>
2021-08-19 11:40:04 +0530

    Create ex-7.py

---
+++ ex-7.py
@@ -0,0 +1,12 @@
+n1=float(input('Enter the number: '))
+n2=float(input('Enter the number: '))
+if abs(n1-n2)<=0.001:
+    print('close')
+else:
+    print('not close')
+    
+#output:
+#Enter the number: 5.001
+#Enter the number: 5.000
+# close
+   
