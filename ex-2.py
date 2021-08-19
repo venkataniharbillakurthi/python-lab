ca1ee127c017d410528dd34f1689ff5d3835a6fd
venkata nihar billakurthi <84034294+venkataniharbillakurthi@users.noreply.github.com>
2021-08-19 10:35:53 +0530

    Update ex-2.py

--- ex-2.py
+++ ex-2.py
@@ -4,3 +4,10 @@ c=int(input('Enter the number:'))
 sum=a+b+c
 average=(a+b+c)/3
 print('sum = ',sum,'\naverage = ',average)
+
+
+Enter the number:23
+Enter the number:45
+Enter the number:67
+sum =  135 
+average =  45.0
